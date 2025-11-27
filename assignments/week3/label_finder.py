1dc239fa667a2e51a08c3bf5bc6396fe5b380c06
LKIndustriesgit <Lennox.Krause@stud.leuphana.de>
2025-11-27 21:29:15 +0100

    finished the lovecrafterator and added a label finder help tool

---
+++ assignments/week3/label_finder.py
@@ -0,0 +1,43 @@
+# I used this file to find fitting words in the lovecraft text.
+# I knew no way to label them so I had to do it kind of manually (still with help of things learned in lesson)
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+from collections import Counter
+nlp = spacy.load('en_core_web_md')
+
+text = open("../week3/hpl").read()
+full_doc = nlp(text)
+
+all_words =  [token for token in full_doc if token.is_alpha]
+#word_count = Counter([w.text for w in all_words])
+#print("most common words: \n")
+#print(word_count.most_common(10))
+
+all_words_without_sw = [word for word in all_words if word.text.lower() not in STOP_WORDS]
+#print(len(all_words), len(all_words_without_sw))
+#word_count = Counter([w.text for w in all_words_without_sw])
+#print("\n most common words (without stop words): \n")
+#print(word_count.most_common(20))
+
+#most_common_adj = [word for word in all_words_without_sw if word.pos_ == "ADJ"]
+#stop_extras = ["great", "certain", "high", "vast", "new", "low", "antarctic", "long", "small", "early", "general", "large", "curious", "upper", "little", "black", "present", "poor", "good", ]
+#STOP_WORDS.update(stop_extras)
+#adj_counter = Counter([w.lemma_ for w in most_common_adj if w.lemma_.lower() not in STOP_WORDS])
+#print("\n most common adjectives: \n")
+#print(adj_counter.most_common(20))
+
+#most_common_noun = [word for word in all_words_without_sw if word.pos_ == "NOUN"]
+#stop_extras = ["foot", "city", "mountain", "camp", "land", "place", "time", "world", "plane", "ice", "sculpture", "course", "wind", "year", "sea", "point", "wall", "rock", "man", ]
+#STOP_WORDS.update(stop_extras)
+#noun_counter = Counter([w.lemma_ for w in most_common_noun if w.lemma_.lower() not in STOP_WORDS])
+#print("\n most common nouns: \n")
+#print(noun_counter.most_common(20))
+
+most_common_verb = [word for word in all_words_without_sw if word.pos_ == "VERB"]
+stop_extras = ["find", "come", "think", "leave", "know", "look", "form", "tell", "begin", "try", "reach", "bring", "rise", "send", "lead", "work", "exist", "feel", "man",
+               "point", "appear", 'suggest','build', 'turn', 'fly', 'accord', 'hold', 'lie', 'decide',
+               ]
+STOP_WORDS.update(stop_extras)
+verb_counter = Counter([w.lemma_ for w in most_common_verb if w.lemma_.lower() not in STOP_WORDS])
+print("\n most common verb: \n")
+print(verb_counter.most_common(20))
---
+++ assignments/week3/lovecrafterator.py
@@ -0,0 +1,99 @@
+import tracery as tr
+import spacy as spacy
+import random
+from tracery.modifiers import base_english
+
+text = open("../week3/hpl").read()
+nlp = spacy.load("en_core_web_md")
+full_doc = nlp(text)
+
+def flatten_subtree(st):
+    return "".join([w.text_with_ws for w in st]).strip()
+
+def phrases_with_word(word):
+    phrases = []
+    for sent in full_doc.sents:
+        if word in sent.text:
+            phrases.append(sent.text.replace("\n", " ").strip())
+    return phrases
+
+
+nouns = set()
+verbs = set()
+adjectives = set()
+
+for token in full_doc:
+    if token.is_stop or token.is_punct or token.is_space:
+        continue
+    if token.pos_ == "NOUN":
+        nouns.add(token.lemma_.lower())
+    elif token.pos_ == "VERB":
+        verbs.add(token.lemma_.lower())
+    elif token.pos_ == "ADJ":
+        adjectives.add(token.lemma_.lower())
+
+
+names = set()
+for ent in full_doc.ents:
+    if ent.label_ in ("PERSON", "ORG"):
+        names.add(ent.text.strip())
+
+
+nouns = list(nouns)
+verbs = list(verbs)
+adjectives = list(adjectives)
+names = list(names)
+##adjs and nouns found through label_finder.py
+NEGATIVE_ADJ_SET = {
+    'monstrous', 'unknown', 'dead', 'strange', 'terrible', 'primal', 'nameless', 'bad','mad', 'fantastic', 'dark',
+}
+
+OLD_ADJ_SET = {
+    "old", "ancient",
+}
+
+MONSTER_NOUN_SET = {
+    "monster", "thing", "specimen", "horror",
+}
+#these were too hard to find, so i used mainly my own
+NEGATIVE_VERB_SET = {
+    "kill", "devour", "haunt", "corrupt", "destroy",
+    "torment", "curse", "encounter", "study", "infect", "consume"
+}
+
+scary_adjs = [a for a in adjectives if a in NEGATIVE_ADJ_SET] #or ["horrible", "hideous"]
+old_adjs = [a for a in adjectives if a in OLD_ADJ_SET] #or ["ancient", "eldritch"]
+monster_nouns = [n for n in nouns if n in MONSTER_NOUN_SET] #or ["monster", "horror", "thing"]
+negative_verbs = [v for v in verbs if v in NEGATIVE_VERB_SET] #or ["devour", "haunt", "destroy"]
+
+deeds = []
+for v in negative_verbs:
+    some_noun = random.choice(nouns)
+    deeds.append(f"{v} the {some_noun}")
+if_phrases = phrases_with_word("If")
+...
+rules = {
+    "origin": "#title# \n #intro# \n #doctor_intro.capitalize#. \n #evil.capitalize#. \n #end#. ",
+    "title": "The #adj# #noun#",
+    "adj": adjectives,
+    "noun": nouns,
+    "intro": if_phrases,
+    "doctor_intro": "I am #education# #name#",
+    "education": ["dr.", "prof.", "sir", "dr. dr.", "king (I recently found out that my family happens to be royal)"],
+    "name": names,
+    "evil": "the #scary# #old# #monster# #did# #stuff#",
+    "scary": scary_adjs,
+    "old": old_adjs,
+    "monster": monster_nouns,
+    "did": ["did", "once did", "secretly did"],
+    "stuff": deeds,
+    "end": "Now I am #consequence#",
+    "consequence": ["dead", "insane"],
+}
+
+grammar = tr.Grammar(rules)
+grammar.add_modifiers(base_english)
+
+for i in range(5):
+    print(grammar.flatten("#origin#"))
+    print("\n --------------- \n")
