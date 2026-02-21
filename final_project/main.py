013a4b32169cc37ee07e513404b4bed355d71c76
LKIndustriesgit <Lennox.Krause@stud.leuphana.de>
2026-02-21 15:05:10 +0100

    added timer logic both to leaderboard and in a reminder if player takes too long. Also added opt-out for tutorial

--- final_project/main.py
+++ final_project/main.py
@@ -60,10 +60,12 @@ class LeaderBoard:
         except json.JSONDecodeError:
             self.entries = []
     def add(self, name, book, score):
-
-        played_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        # played_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        #minutes = elapsed // 60
+        #seconds = round(elapsed % 60, 1)
+        #timer = (minutes, seconds)
         self.entries.append(
-            {"name": name, "book": book, "clues": score, "time": played_at}
+            {"name": name, "book": book, "clues": score, "time": elapsed, }
         )
         self.entries.sort(key=lambda e: (-e["clues"], e["time"]), reverse=True)
         with open("leaderboard.json", "w", encoding="utf-8") as f:
@@ -72,7 +74,11 @@ class LeaderBoard:
     def show(self):
         print("––– LEADERBOARD –––")
         for i, e in enumerate(self.entries, start=1):
-            print(f"{i}. {e['name']} | {e['book']} | clues: {e['clues']} | time: {e['time']}")
+            t = e.get("time", 0)
+            minutes = int(t // 60)
+            seconds = round(t % 60, 1)
+            time_str = f"{minutes}:{seconds:5.1f}"
+            print(f"{i}. {e['name']} | {e['book']} | clues= {e['clues']} | time= {time_str}")
 
 def nearest_neighbors(target, vocab, k=5):
     query = nlp(target)[0]
@@ -145,44 +151,52 @@ def play_game():
     text_model = markovify.Text( anonymized_text, state_size=3)
     print("++++++++++++++++++++++++++++++++++")
     print("Welcome to the Book Guessing Game!")
-    time.sleep(TICK)
-    print("You will have to guess a random book out of 5 available by the tone used")
-    time.sleep(TICK)
-    print("You can gain insights through different actions, each penalising you with clue points")
-    time.sleep(TICK)
-    print("The less clue points you have, the better")
-    time.sleep(TICK)
-    print("1. Action: Entering words to the AI. It will find 5 words out of the book semantically most related to it")
-    time.sleep(TICK)
-    print("The NLP AI will assign a number to these words based on how strongly it thinks they fit to the word")
-    time.sleep(TICK)
-    print("Example:")
-    print("word = house. Words by AI = 'Mansion' (1.003), 'castle' (0.921), \n 'room' (0.850), 'shed' (0.842), 'live' (0.770)")
-    print("conclusion: The book has a setting with castles and mansions in it")
-    time.sleep(TICK)
-    print("stop words (common non-descriptive words like 'I' and 'the') are filtered out of the book, as they do not add any evidence")
-    time.sleep(TICK)
-    print("This is worth 0.5 clues per use")
-    time.sleep(TICK)
-    print("Alternatively, you can let the game generate a sentence containing words of the book")
-    time.sleep(TICK)
-    print("The sentence is nonsentical, but it can give you insight on the books tone (its worth 1 clue)")
-    time.sleep(TICK)
-    print("finally, you can let the program list you the 5 most used non-stop words of the book (2 clues penalty)")
-    time.sleep(TICK)
-    print("You didnt understand this tutorial?")
-    time.sleep(TICK * 0.5)
-    print("Well, just try learning by doing, I guess!")
-    print("List of books:")
-    print(books)
-    print("You can add your own books into the 'book' folder for more replayability")
-    time.sleep(TICK)
-    print("(some features may not be as optimised for this)")
-
+    tut = True
+    while tut:
+        tutorial = input("Do you want a tutorial? (y/n)")
+        if tutorial == "y":
+            print("You will have to guess a random book out of 5 available by the tone used")
+            time.sleep(TICK)
+            print("You can gain insights through different actions, each penalising you with clue points")
+            time.sleep(TICK)
+            print("The less clue points you have, the better")
+            time.sleep(TICK)
+            print("1. Action: Entering words to the AI. It will find 5 words out of the book semantically most related to it")
+            time.sleep(TICK)
+            print("The NLP AI will assign a number to these words based on how strongly it thinks they fit to the word")
+            time.sleep(TICK)
+            print("Example:")
+            print("word = house. Words by AI = 'Mansion' (1.003), 'castle' (0.921), \n 'room' (0.850), 'shed' (0.842), 'live' (0.770)")
+            print("conclusion: The book has a setting with castles and mansions in it")
+            time.sleep(TICK)
+            print("stop words (common non-descriptive words like 'I' and 'the') are filtered out of the book, as they do not add any evidence")
+            time.sleep(TICK)
+            print("This is worth 0.5 clues per use")
+            time.sleep(TICK)
+            print("Alternatively, you can let the game generate a sentence containing words of the book")
+            time.sleep(TICK)
+            print("The sentence is nonsentical, but it can give you insight on the books tone (its worth 1 clue)")
+            time.sleep(TICK)
+            print("finally, you can let the program list you the 5 most used non-stop words of the book (2 clues penalty)")
+            time.sleep(TICK)
+            print("You didnt understand this tutorial?")
+            time.sleep(TICK * 0.5)
+            print("Well, just try learning by doing, I guess!")
+            print("List of books:")
+            print(books)
+            print("You can add your own books into the 'book' folder for more replayability")
+            time.sleep(TICK)
+            print("(some features may not be as optimised for this)")
+            break
+        elif tutorial == "n":
+            break
+        else:
+            print("Unknown command, try again.")
 
     result = False
     word_count = Counter(
         [token.text.lower() for token in doc if not token.is_stop and token.is_alpha and token.has_vector])
+    start = time.perf_counter()
     while flag:
 
         print("Clues used: " + str(clues))
@@ -190,6 +204,8 @@ def play_game():
         if word == "s":
             result = True
             flag = False
+            global elapsed
+            elapsed = time.perf_counter() - start
         if word == "c":
             #print(generate_sentence())
             print(text_model.make_sentence(tries=100))
@@ -208,6 +224,8 @@ def play_game():
                 neighbor = nearest_neighbors(word, vocab_from_file, 5)
                 print(neighbor)
                 clues += 0.5
+        if time.perf_counter() - start > 300 and not result:
+            print("HURRY UP! You are too slow.")
 
 
     while result:
